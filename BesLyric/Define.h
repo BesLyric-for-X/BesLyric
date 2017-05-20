e97a234c5420fdd38255be83e24d08d75c844531
BensonLaur <1035069088@qq.com>
2017-05-20 14:41:34 +0800

    支持windows除ascii码外的其他三种编码：Unicode little endian、 Unicode big endian、 UTF-8

---
+++ BesLyric/Define.h
@@ -0,0 +1,39 @@
+/*
+	BesLyric  一款 操作简单、功能实用的 专门用于制作网易云音乐滚动歌词的 歌词制作软件。
+    Copyright (C) 2017  BensonLaur
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+* @file       CodeFile.h
+* @version    v1.0      
+* @author     BensonLaur   
+* @date       2017/05/19
+* 
+* Describe    定义了 各种编码文件 的操作	
+*/
+
+#pragma once
+#include "stdafx.h"
+using namespace std;
+
+enum ENCODING_TYPE			//文件编码类型
+{
+	ASCII,						
+	UNICODE_LITTLE_ENDIAN,
+	UNICODE_BIG_ENDIAN,
+	UTF_8,
+	OTHER
+};
